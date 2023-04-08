479508c9938d5f813285a1ccbce793df07d848d0
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-04-08 15:22:23 +0900

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by SHIRAISHI HIROYUKI on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
